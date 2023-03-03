e7e006084135851900275ce5ba5bda15dbd647fd
MorvidAngel <5d7n2s9m6b@live.com>
2023-03-02 21:32:57 -0500

    new notes

---
+++ Classes/section2/video9/script.js
@@ -0,0 +1,93 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Jonas';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+camel case is when you capitalize the words exept for the first word
+object oriented programming
+
+let country = 'United States';
+let continent = 'North America';
+let populatioin = '331.9 million';
+
+console.log(country);
+console.log(continent);
+console.log(populatioin);
+*/
+// number
+/*String
+Boolean
+undefined
+null
+symbol
+bigInt
+*/
+
+// let javasrciptIsFun = true;
+// console.log(javasrciptIsFun);
+
+// video 13----------------------------------------------------------------------------
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// birthYear = 1990;
+// console.log(birthYear);
+
+// variables: let, const and variables
+
+// video 14------------------------------------------------------------------------------
+
+// oporators:
+// const now = 2037;
+// const ageJones = now - 1991;
+// const ageSona = now - 2018;
+// console.log(ageJones, ageSona);
+
+// console.log(ageJones * 2, ageJones / 10, 2 ** 3);
+
+// const firstName = 'Jonas';
+// const lastName = 'Perez';
+// console.log(firstName + ' ' + lastName);
+
+// typeof
+
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// console.log(x);
+
+// video 15-------------------------------------------------------------------------------
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
+
+// video 16 coding challange#1----------------------------------------------------------------
+// var bmi = prompt('What is your bmi?');
+// var height = prompt('What is your height?');
+
+// println('Hi' + bmi + '!');
+
+const markmass = 78;
+const markheight = 1.69;
+const johnmass = 92;
+const johnheight = 1.95;
+
+const BMIMark = markmass / markheight ** 2;
+const BMIJohn = johnmass / (johnheight * johnheight);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
